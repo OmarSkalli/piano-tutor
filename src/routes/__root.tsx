ad7ce271dee21ca968592831682d2c6f3fed4a7c
Omar Skalli <omar.skalli7@gmail.com>
2026-03-26 23:17:28 -0700

    Add CSS loading bar that animates before React mounts

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,13 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { useEffect } from 'react'
+
+function Root() {
+  useEffect(() => {
+    document.getElementById('loading-bar')?.remove()
+  }, [])
+  return <Outlet />
+}
 
 export const Route = createRootRoute({
-  component: () => <Outlet />,
+  component: Root,
 })
